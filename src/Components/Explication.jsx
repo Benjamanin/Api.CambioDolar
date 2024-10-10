19ecedcbd81e46c58a8ddcf4a669d3ffd9cf6916
MartinHernandez <martinignacio.hernandez1@alumnos.ulagos.cl>
2024-10-09 22:46:34 -0300

    Update Explication.jsx

--- src/Components/Explication.jsx
+++ src/Components/Explication.jsx
@@ -11,16 +11,16 @@ export function Explication() {
       <Grid.Col span={3} style={{ minHeight: rem(80) }}>
       <Paper shadow="xs" p="xl">
       <Text size='xl 'ta="center" fw={700}>
-      ¿Que significa el ultimo cierre </Text>
+      ¿Que significa el último cierre ?</Text>
       <Text>
-      El último cierre es el valor final al que una moneda se intercambió al final del último día hábil de operaciones en el mercado, y sirve como referencia clave para evaluar su rendimiento y tendencia futura.
+      El último cierre es el valor final al que una moneda se intercambió al final del último día hábil de operaciones en el mercado y sirve como referencia para evaluar su rendimiento y tendencia futura.
       </Text>
     </Paper> 
    </Grid.Col>
       <Grid.Col span={3} style={{ minHeight: rem(120) }}>
       <Paper shadow="xs" p="xl">
       <Text size='xl 'ta="center" fw={700}>
-      ¿Que significa la fecha de Actualizacion? </Text>
+      ¿Que significa la fecha de actualizacion? </Text>
       <Text>
         La fecha de actualización indica el último momento en que cambió la cotización de la moneda en el mercado, reflejando su valor más reciente disponible.
       </Text>
